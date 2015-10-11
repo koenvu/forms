95995ef4d86d697f5906a2e2311dc3560a4c41c7
Koen van Urk <git@ehlo-localhost.com>
2015-10-11 15:47:36 +0200

    Rename id variable

--- src/Forms/Components/Labeller.php
+++ src/Forms/Components/Labeller.php
@@ -12,23 +12,24 @@ trait Labeller
      *
      * @param [FormElement] $fields Array of FormElement objects
      * @param boolean $generateIds Should fields automatically get an ID tag added if they do not exist?
+     * @param callback $generatorCallback This callback will be used to generate an ID when required
      */
-    public function enhanceLabels($fields = [], $generateIds = false)
+    public function enhanceLabels($fields = [], $generateIds = false, $generatorCallback = 'spl_object_hash')
     {
         foreach ($fields as $field) {
             if ($field->get('field.id') === null && $generateIds) {
                 // Attempt to generate the id based on the name
                 // option if it exists, or generate the hash
                 // of the field object as fallback name.
-                $id = $field->get('field.name', spl_object_hash($field));
-                $field->set('field.id', $id);
+                $identifier = $field->get('field.name', call_user_func($generatorCallback, $field));
+                $field->set('field.id', $identifier);
             }
 
             // Now try to retrieve the ID again and assign it
             // to the label.for attribute if it exists. If
             // we just generated an ID, it surely will.
-            if ($id = $field->get('field.id')) {
-                $field->set('label.for', $id);
+            if ($identifier = $field->get('field.id')) {
+                $field->set('label.for', $identifier);
             }
         }
     }
